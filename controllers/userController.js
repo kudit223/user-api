cb3f6e55044f084240fd02a0cc9d507a56c26794
Udit Kumar <kudit223@gmail.com>
2026-06-11 01:14:09 +0530

    cors origin  updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,7 +119,8 @@ exports.loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      sameSite: "strict",
+      secure: true,
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
