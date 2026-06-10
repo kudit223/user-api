813f6208f5c52e97c06fc16d37ad9615e9f27a94
Udit Kumar <kudit223@gmail.com>
2026-06-10 23:36:24 +0530

    cookie updated

--- controllers/userController.js
+++ controllers/userController.js
@@ -119,7 +119,7 @@ exports.loginUser = async (req, res) => {
 
     res.cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
